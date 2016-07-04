9f47a31f9fcce577c759399fe43251092454b992
tyfkda <tyfkda@gmail.com>
2016-07-04 10:13:58 +0900

    Insert white spaces same as previous line

--- src/processing/mode/ruby/RubyInputHandler.java
+++ src/processing/mode/ruby/RubyInputHandler.java
@@ -1,12 +1,16 @@
 package processing.mode.ruby;
 
 import java.awt.event.KeyEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import processing.app.syntax.JEditTextArea;
 import processing.app.syntax.PdeInputHandler;
 import processing.app.ui.Editor;
 
 public class RubyInputHandler extends PdeInputHandler {
+  private static Pattern reIndent = Pattern.compile("^(\\s+)");
+
   private final RubyEditor rbEditor;
 
   public RubyInputHandler(final Editor editor) {
@@ -24,7 +28,12 @@ public class RubyInputHandler extends PdeInputHandler {
   }
 
   private String newline() {
-    return "\n";
+    final JEditTextArea textArea = getTextArea();
+    final String line = textArea.getLineText(textArea.getCaretLine());
+    Matcher m = reIndent.matcher(line);
+    if (!m.find())
+      return "\n";
+    return "\n" + m.group(1);
   }
 
   private JEditTextArea getTextArea() {
