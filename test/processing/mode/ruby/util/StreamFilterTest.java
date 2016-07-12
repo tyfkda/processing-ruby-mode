39e94a6fa1617b3a0fec80504b3b431958b86ef8
tyfkda <tyfkda@gmail.com>
2016-07-13 05:31:43 +0900

    Add a unit test to check buffer is expanded

--- test/processing/mode/ruby/util/StreamFilterTest.java
+++ test/processing/mode/ruby/util/StreamFilterTest.java
@@ -1,6 +1,6 @@
 package processing.mode.ruby.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class StreamFilterTest {
   private static void putString(OutputStream stream, String string) throws IOException {
     for (char ch : string.toCharArray()) {
-      stream.write((int) ch);
+      stream.write(ch);
     }
   }
 
@@ -31,7 +31,20 @@ public class StreamFilterTest {
     putString(filter, "short\n");
     putString(filter, "longlonglong\n");
     putString(filter, "middle\n");
-
     assertEquals(ostream.toString(), "short\nlonglonglong\nmiddle\n");
+  }
+
+  @Test
+  public void testBufferExpand() throws IOException {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < 100; ++i)
+      sb.append("0123456789");
+    sb.append("\n");
+    String longString = sb.toString();
+
+    ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+    StreamFilter filter = new StreamFilter(ostream, Pattern.compile("x"), "");
+    putString(filter, longString);
+    assertEquals(ostream.toString(), longString);
   }
 }
